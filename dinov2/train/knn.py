4ed70dc8ce71cc80a0e5aa33ab5025a3c9a7e4fb
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2026-02-03 12:06:02 +0100

    evaluation

--- dinov2/train/knn.py
+++ dinov2/train/knn.py
@@ -3,6 +3,9 @@
 # This source code is licensed under the Apache License, Version 2.0
 # found in the LICENSE file in the root directory of this source tree.
 
+import os
+import sys
+
 sys.path.append(".")
 
 import argparse
